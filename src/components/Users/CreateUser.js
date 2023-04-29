95535cb5ad778ed1921fb092587cf0cb1b08b66c
SvetaLantsman <svetlana@curalife.com>
2023-04-29 20:37:13 +0300

    .

--- src/components/Users/CreateUser.js
+++ src/components/Users/CreateUser.js
@@ -4,6 +4,7 @@ import React, {useState} from "react";
 const CreateUser =(props)=>{
     const [inputName, setInputName] = useState("");
     const [inputAge, setInputAge] = useState("");
+    const [error, setError] = useState();
 
 
     const nameChangeHandler =(event)=>{
@@ -13,16 +14,31 @@ const CreateUser =(props)=>{
         setInputAge(event.target.value);
     }
 
-    const createUserHandler = (event)=>{
-        event.preventDefault();
-        props.onCreateUser(inputName, inputAge);
-        setInputName("");
-        setInputAge("");
-    }
-
+    const createUserHandler = (event) => {
+      event.preventDefault();
+      if (inputName.trim().length === 0 || inputAge.trim().length === 0) {
+        setError({
+          title: "Incorrect Input",
+          message: "These fields cannot be empty",
+        });
+        return;
+      }
+      if (+inputAge < 1) {
+        setError({
+          title: "Incorrect Age",
+          message: "The age should be greater than 0",
+        });
+        return;
+      }
+      // console.log(inputName, inputAge);
+      props.onCreateUser(inputName, inputAge);
+      setInputName("");
+      setInputAge("");
+    };
 
     return (
         <div className={styles.input}>
+            {error && (<h2>{error.title} {error.message}</h2>)}
             <form onSubmit={createUserHandler}>
                 <label>Name</label>
                 <input type="text" id="name" value={inputName} onChange={nameChangeHandler}></input>
